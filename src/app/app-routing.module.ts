f0eb933e939b8ce229f39c1cf7e69c37d266f1b0
HarshaPrajapati <harsha_1605@yahoo.com>
2020-09-27 23:28:53 +0530

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChallengesComponent } from './challenges/challenges.component';
 import { HomeComponent } from './home/home.component';
+import { NewRegistrationComponent } from './new-registration/new-registration.component';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { AuthGuard } from './_pipes/auth.guard';
 
 const routes: Routes = [
-  {path:'userLogin' , component:UserLoginComponent},
-  
-  {path:'home' , component:HomeComponent},
+  {path:'' , component:HomeComponent , canActivate :[AuthGuard]},
+  {path:'userLogin' , component:UserLoginComponent },
+  {path:'register' , component: NewRegistrationComponent },
   {path:'challenge' , component:ChallengesComponent},
   
 ];
