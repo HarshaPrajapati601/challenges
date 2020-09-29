4653bbec5b43b7c2013c243e6eedc5a7eceaeb87
HarshaPrajapati <harsha_1605@yahoo.com>
2020-09-29 23:18:01 +0530

    likes ,upvotes updated and added in firebase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import { element } from 'protractor';
 import { AuthenticationService } from '../authentication.service';
 import { Challenge } from '../_models/challenge';
 import { ChallengDataService } from '../_service/challeng-data.service';
+import { db } from '../_service/firebase.js';
 
 @Component({
   selector: 'app-home',
@@ -28,7 +29,12 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.dataSource =JSON.parse(localStorage.getItem("challenges"));
+    let stored = JSON.parse(localStorage.getItem("challenges"));
+    if(stored){
+      this.dataSource = JSON.parse(localStorage.getItem("challenges"));
+    }else{
+      this.dataSource=[];
+    }
     this.activatedRoute.params
     .subscribe((params : Params)=>{
       console.log("para",params)
@@ -43,26 +49,52 @@ export class HomeComponent implements OnInit {
   }
 //Users who upvote for a challenge
   upVoteChallenge(userRegister){
-    let challengesObj = JSON.parse(localStorage.getItem("challenges")) || {};
+    let challengesObj = JSON.parse(localStorage.getItem("challenges")) || [];
     for (var i = 0; i < challengesObj.length; i++) {
       if(challengesObj[i].usersUpVoted.length == 0  && challengesObj[i].challengeId == userRegister.challengeId){  
         challengesObj[i].usersUpVoted.push(this.currentUserDetails);
         userRegister.usersUpVoted = challengesObj[i].usersUpVoted;
+        db.collection("challenges").doc(`${challengesObj[i].challengeId}`)
+          .update({
+            usersUpVoted: challengesObj[i].usersUpVoted
+          });
         localStorage.setItem("challenges", JSON.stringify(challengesObj));
       }
       else if(challengesObj[i].usersUpVoted.length > 0 && challengesObj[i].challengeId == userRegister.challengeId){
         if((challengesObj[i].usersUpVoted).includes(this.currentUserDetails) == false){
           userRegister.usersUpVoted = challengesObj[i].usersUpVoted;
             challengesObj[i].usersUpVoted.push(this.currentUserDetails);
+            db.collection("challenges").doc(`${challengesObj[i].challengeId}`)
+            .update({
+              usersUpVoted: challengesObj[i].usersUpVoted
+            });
             localStorage.setItem("challenges", JSON.stringify(challengesObj));
         }
       }
     }
-    localStorage.getItem("challenges");
+  }
+  //users downVote challenge
+  downVoteChallenge(userRegister){
+    let challengesObj = JSON.parse(localStorage.getItem("challenges")) || [];
+    for (var i = 0; i < challengesObj.length; i++) {
+      if(challengesObj[i].usersUpVoted.length > 0 && challengesObj[i].challengeId == userRegister.challengeId){
+        if((challengesObj[i].usersUpVoted).includes(this.currentUserDetails)){
+         let index = challengesObj[i].usersUpVoted.findIndex(obj=> obj ==this.currentUserDetails);
+            challengesObj[i].usersUpVoted.splice(index ,1);
+            userRegister.usersUpVoted = challengesObj[i].usersUpVoted ;  
+            localStorage.setItem("challenges", JSON.stringify(challengesObj));
+            db.collection("challenges").doc(`${challengesObj[i].challengeId}`)
+            .update({
+              usersUpVoted: challengesObj[i].usersUpVoted
+            });
+            this.dataSource[i] = challengesObj[i] ;
+        }
+      }
+    }
   }
   
    addDataToChallengesArray(ele , direction : boolean) {
-    let challengesObj = JSON.parse(localStorage.getItem("challenges")) || {};
+    let challengesObj = JSON.parse(localStorage.getItem("challenges")) || [];
             // get the current challengesObj, or an empty object if null  
       if(direction == false){
         for (var i = 0; i < challengesObj.length; i++) {
@@ -71,26 +103,41 @@ export class HomeComponent implements OnInit {
             ele.voteCount = challengesObj[i].voteCount;   
             challengesObj[i].usersVoted.push(this.currentUserDetails);
             localStorage.setItem("challenges", JSON.stringify(challengesObj));
+            db.collection("challenges").doc(`${challengesObj[i].challengeId }`)
+            .update({
+              usersVoted: challengesObj[i].usersVoted,
+              voteCount: challengesObj[i].voteCount 
+            })
           }
           else if(challengesObj[i].usersVoted.length > 0 && challengesObj[i].challengeId == ele.challengeId){
                 if((challengesObj[i].usersVoted).includes(this.currentUserDetails) == false){
                   challengesObj[i].voteCount++ ;
                   ele.voteCount = challengesObj[i].voteCount ;  
-                    challengesObj[i].usersVoted.push(this.currentUserDetails);
-                    localStorage.setItem("challenges", JSON.stringify(challengesObj));
+                  challengesObj[i].usersVoted.push(this.currentUserDetails);
+                  localStorage.setItem("challenges", JSON.stringify(challengesObj));
+                  db.collection("challenges").doc(`${challengesObj[i].challengeId }`)
+                  .update({
+                    usersVoted: challengesObj[i].usersVoted,
+                    voteCount: challengesObj[i].voteCount 
+                  })
                 }
           }
-        }  // put the result back in localStorage and update datasource
+        }  // put the result back in localStorage and update datasource   
     }
     else{
         for (var i = 0; i < challengesObj.length; i++) {
           if(challengesObj[i].usersVoted.length > 0 && challengesObj[i].challengeId == ele.challengeId){
                 if((challengesObj[i].usersVoted).includes(this.currentUserDetails)){
                  let index = challengesObj[i].usersVoted.findIndex(obj=> obj ==this.currentUserDetails);
-                  challengesObj[i].voteCount-- ;
-                   ele.voteCount =  challengesObj[i].voteCount;  
                     challengesObj[i].usersVoted.splice(index ,1);
+                    challengesObj[i].voteCount-- ;
+                    ele.voteCount = challengesObj[i].voteCount ;  
                     localStorage.setItem("challenges", JSON.stringify(challengesObj));
+                    db.collection("challenges").doc(`${challengesObj[i].challengeId }`)
+                    .update({
+                      usersVoted: challengesObj[i].usersVoted,
+                      voteCount: challengesObj[i].voteCount 
+                    })
                 }
           }
         }
@@ -102,7 +149,6 @@ export class HomeComponent implements OnInit {
       //count gets incremented in challenges array
      this.addDataToChallengesArray(ele ,false ) ;
      this.likes =false;  
-     this.dataSource =JSON.parse(localStorage.getItem("challenges"));
      //the userId gets added in challenges array
   
   }
@@ -110,7 +156,6 @@ export class HomeComponent implements OnInit {
   onDisLikeToggle(ele){
       this.addDataToChallengesArray(ele ,true ) ;
       this.likes =true; 
-      this.dataSource =JSON.parse(localStorage.getItem("challenges"));
   }
 
 }
